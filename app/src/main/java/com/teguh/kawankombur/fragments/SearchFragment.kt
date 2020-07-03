83c0da1bc1380ae8760447eafb99aedaab5ee9c6
Teguh Kurniawan <teguhkrniawan@gmail.com>
2020-07-03 21:00:24 +0700

    Menambahkan fitur cari data di search fragment

--- app/src/main/java/com/teguh/kawankombur/fragments/SearchFragment.kt
+++ app/src/main/java/com/teguh/kawankombur/fragments/SearchFragment.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -45,6 +46,22 @@ class SearchFragment : Fragment() {
         search_recyclerview.adapter = userAdapter
 
         initView()
+
+        searh_user_edit.addTextChangedListener(object: TextWatcher{
+            override fun afterTextChanged(p0: Editable?) {
+
+            }
+
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+
+            }
+
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                setUsersList.clear()
+                filterData(p0.toString())
+            }
+
+        })
     }
 
     // function tampil semua users
@@ -58,13 +75,39 @@ class SearchFragment : Fragment() {
             override fun onDataChange(dataSnapshot: DataSnapshot) {
                 for (item in dataSnapshot.children){
                     val user:  Users? = item.getValue(Users::class.java)
-                    setUsersList.add(user!!)
-                    userAdapter.setUsers(setUsersList)
+                    if (user!!.getUID() != userUID){
+                        setUsersList.add(user)
+                        userAdapter.setUsers(setUsersList)
+                    }
                 }
             }
+        })
+    }
 
+    private fun filterData(name: String){
+
+        val querySearch = FirebaseDatabase.getInstance().reference.child("Users")
+            .orderByChild("search")
+            .startAt(name)
+            .endAt(name + "\uf8ff") // asumsi sementara uf8ff itu unicode pemgambil karakter, misalkan ketik fre, maka hasilnya fredy, freya,frecuk
+
+        querySearch.addValueEventListener(object : ValueEventListener{
+            override fun onCancelled(p0: DatabaseError) {
+                Toast.makeText(context, "error pada fragment search addValueListener", Toast.LENGTH_SHORT).show()
+            }
+
+            override fun onDataChange(dataSnapshot: DataSnapshot) {
+                for (item in dataSnapshot.children){
+                    val user:  Users? = item.getValue(Users::class.java)
+                    if (user!!.getUID() != userUID){
+                        setUsersList.add(user)
+                        userAdapter.setUsers(setUsersList)
+                    }
+                }
+            }
         })
 
+        Log.i("list", "empty? : "+setUsersList.isEmpty())
 
     }
 
